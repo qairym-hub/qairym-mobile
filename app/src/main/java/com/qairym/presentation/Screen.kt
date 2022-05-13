6d70f65de7a9ed728fd967558f432adf2cd34638
Armani020 <ar.amirgaliev.2015@gmail.com>
2022-05-13 09:39:52 +0600

    The right version of navigation and bottom navigaion bar

--- app/src/main/java/com/qairym/presentation/Screen.kt
+++ app/src/main/java/com/qairym/presentation/Screen.kt
@@ -1,8 +1,9 @@
 package com.qairym.presentation
 
 sealed class Screen(val route: String) {
-    object LoginScreen: Screen("login_screen")
-    object RegistrationScreen: Screen("registration_screen")
-    object HomeScreen: Screen("home_screen")
-    object ProfileScreen: Screen("profile_screen")
+    object LoginScreen: Screen("login")
+    object RegistrationScreen: Screen("registration")
+    object MainScreen: Screen("main")
+//    object HomeScreen: Screen("home_screen")
+//    object ProfileScreen: Screen("profile_screen")
 }
--- app/src/main/java/com/qairym/presentation/home/ProfileScreen.kt
+++ app/src/main/java/com/qairym/presentation/home/ProfileScreen.kt
@@ -2,30 +2,18 @@ package com.qairym.presentation.home
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavController
-import com.qairym.presentation.home.bottomnavbar.BottomBar
-import com.ramcosta.composedestinations.annotation.Destination
 
-@Destination
 @Composable
-fun ProfileScreen(
-    navController: NavController,
-) {
-    Scaffold(
-        bottomBar = {
-            BottomBar(navController = navController)
-        }
+fun ProfileScreen() {
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
     ) {
-        Box(
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
-        ) {
-            Text(text = "Profile Screen!")
-        }
+        Text(text = "Profile Screen!")
     }
 }
